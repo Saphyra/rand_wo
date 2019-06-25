0d34b94d8b73323746799efdbab76ed3b2171740
Bernat Bonda <bernat_bonda@epam.com>
2019-06-25 08:27:37 +0200

    Labels -> show items

--- src/main/java/com/github/saphyra/randwo/page/ListItemsController.java
+++ src/main/java/com/github/saphyra/randwo/page/ListItemsController.java
@@ -1,10 +1,19 @@
 package com.github.saphyra.randwo.page;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.github.saphyra.randwo.key.domain.Key;
+import com.github.saphyra.randwo.key.repository.KeyDao;
+import com.github.saphyra.randwo.label.domain.Label;
+import com.github.saphyra.randwo.label.repository.LabelDao;
 import com.github.saphyra.randwo.page.component.ModelAndViewFactory;
+import com.github.saphyra.randwo.page.domain.ModelAttribute;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,11 +23,20 @@ import lombok.extern.slf4j.Slf4j;
 public class ListItemsController {
     private static final String LIST_ITEMS_MAPPING = "/items";
 
+    private final KeyDao keyDao;
+    private final LabelDao labelDao;
     private final ModelAndViewFactory modelAndViewFactory;
 
     @GetMapping(LIST_ITEMS_MAPPING)
-    ModelAndView listItems() {
+    ModelAndView listItems(
+        @RequestParam(value = "label", required = false) UUID labelId,
+        @RequestParam(value = "key", required = false) UUID keyId
+    ) {
         log.info("Request arrived to {}", LIST_ITEMS_MAPPING);
-        return modelAndViewFactory.create("list_items");
+        return modelAndViewFactory.create(
+            "list_items",
+            new ModelAttribute("label", Optional.ofNullable(labelId).flatMap(labelDao::findById).map(Label::getLabelValue).orElse("")),
+            new ModelAttribute("key", Optional.ofNullable(keyId).flatMap(keyDao::findById).map(Key::getKeyValue).orElse(""))
+        );
     }
 }
